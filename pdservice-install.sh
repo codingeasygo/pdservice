2d233f0aaf35f6fd4ffe9e467b80dda68a71e604
centny <centny@gmail.com>
2021-06-13 19:40:05 +0800

    revert.

--- pdservice-install.sh
+++ pdservice-install.sh
@@ -2,17 +2,22 @@
 set -xe
 case "$1" in
   -i)
+    if [ ! -d /home/pdsd ];then
+      useradd pdsd
+      mkdir -p /home/pdsd
+      chown -R pdsd:pdsd /home/pdsd
+    fi
     if [ ! -f /etc/systemd/system/pdservice.service ];then
        cp -f pdservice.service /etc/systemd/system/
     fi
-    if [ ! -f /root/pdsd/conf/pdservice.properties ];then
-      mkdir -p /root/pdsd/conf/
-      cp -f conf/pdservice.properties /root/pdsd/conf/
+    if [ ! -f /home/pdsd/conf/pdservice.properties ];then
+      mkdir -p /home/pdsd/conf/
+      cp -f conf/pdservice.properties /home/pdsd/conf/
     fi
-    rm -rf /root/pdsd/pdservice
-    mkdir -p /root/pdsd/pdservice
-    cp -rf * /root/pdsd/pdservice
-    chown -R pdsd:pdsd /root/pdsd
+    rm -rf /home/pdsd/pdservice
+    mkdir -p /home/pdsd/pdservice
+    cp -rf * /home/pdsd/pdservice
+    chown -R pdsd:pdsd /home/pdsd
     systemctl enable pdservice.service
     ;;
   *)
